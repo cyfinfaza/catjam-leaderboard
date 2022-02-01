262aaf85fed33a2cbf44437386535b610ecea442
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2022-01-31 19:50:47 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+from flask import Flask, request, Response, render_template
+from flask_cors import CORS
+import pymongo
+import dotenv
+from os import environ
+import json
+
+dotenv.load_dotenv()
+MONGODB_SECRET = environ.get("PVT_MONGODB")
+
+client = pymongo.MongoClient(MONGODB_SECRET)
+db = client.analytics
+leaderboardCollection = db.leaderboard
+
+app = Flask(__name__)
+cors = CORS(app, supports_credentials=True)
+app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
+
+
+def success_json(data=None):
+    if data != None:
+        return Response(
+            json.dumps({"status": "success", "data": data}),
+            status=200,
+            mimetype="application/json",
+        )
+    else:
+        return Response(
+            json.dumps({"status": "success"}), status=200, mimetype="application/json"
+        )
+
+
+def error_json(error=None):
+    if error != None:
+        return Response(
+            json.dumps({"status": "error", "error": error}),
+            status=400,
+            mimetype="application/json",
+        )
+    else:
+        return Response(
+            json.dumps({"status": "error"}), status=400, mimetype="application/json"
+        )
+
+
+def fetchLeaderboard():
+    leaderboard = (
+        leaderboardCollection.find().sort("score", pymongo.DESCENDING).limit(5)
+    )
+    leaderboard = [
+        {"score": x["score"], "initials": x["initials"]} for x in leaderboard
+    ]
+    return leaderboard
+
+
+@app.route("/")
+def index():
+    return render_template("leaderboard.html", scores=fetchLeaderboard())
+
+
+@app.route("/reportScore", methods=["POST"])
+def report():
+    data = request.get_json(force=True)
+    if data and "score" in data and "initials" in data:
+        if not (isinstance(data["initials"], str) and isinstance(data["score"], int)):
+            return error_json("Invalid types, must be string and int")
+        if len(data["initials"]) != 3:
+            return error_json("Initials must be 3 characters or less")
+        if data["score"] < 0 or data["score"] > 999:
+            return error_json("Score must be between 0 and 999")
+        numAboveScore = leaderboardCollection.count_documents(
+            {"score": {"$gte": data["score"]}}
+        )
+        if numAboveScore > 0:
+            return error_json("Score must be greater than the top score")
+        else:
+            leaderboardCollection.insert_one(
+                {
+                    "score": data["score"],
+                    "initials": data["initials"].upper(),
+                    "timestamp": datetime.utcnow(),
+                }
+            )
+            return success_json()
+    else:
+        return error_json("Malformed request")
+
+
+@app.route("/leaderboard")
+def leaderboard():
+    return success_json(fetchLeaderboard())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
